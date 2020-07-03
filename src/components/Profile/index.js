7f8dd7dec1c764b11e0cff62b7a8580822c65838
djair <djhanlj@gmail.com>
2020-07-03 15:11:37 -0300

    estrutura inicial

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Profile = () => (
+
+    <div className="profile-helper">
+        <h1>Djair Soares</h1>
+        <h2>Programador</h2>
+        <p>Desenvolvedor de sistemas tentando evoluir</p>    
+    </div>
+);
+
+export default Profile;
