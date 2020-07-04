d9fca15f8c71c3a26ded809ac1bcdf57c323e599
djair <djhanlj@gmail.com>
2020-07-04 12:50:29 -0300

    listagem de artigos dos arquivos markdown

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import * as S from './styled'
 
-const PostItem = () => (
-    <S.PostItemLink to="/slug/">
-      <S.PostItemWrapper>
-        <S.PostItemTag background="#47650b">Misc</S.PostItemTag>
-        <S.PostItemInfo>
-          <S.PostItemDate>30 de Julho de 2019 • 4 min de leitura</S.PostItemDate>
-          <S.PostItemTitle>
-            Diga não ao Medium: tenha sua própria plataforma
-          </S.PostItemTitle>
-          <S.PostItemDescription>
-            Algumas razões para você ter sua própria plataforma ao invés de
-            soluções como o Medium.
-          </S.PostItemDescription>
-        </S.PostItemInfo>
-      </S.PostItemWrapper>
+const PostItem = ({ background, slug, category, date, timeToRead, title, description }) => (
+    <S.PostItemLink to={slug}>
+        <S.PostItemWrapper>
+            <S.PostItemTag background={background}>{category}</S.PostItemTag>
+            <S.PostItemInfo>
+                <S.PostItemDate>{date}  {timeToRead} min de leitura</S.PostItemDate>
+                <S.PostItemTitle>
+                    {title}
+                </S.PostItemTitle>
+                <S.PostItemDescription>
+                    {description}
+                </S.PostItemDescription>
+            </S.PostItemInfo>
+        </S.PostItemWrapper>
     </S.PostItemLink>
-  )
-  
+)
+
+PostItem.propTypes = {
+    background: PropTypes.string,
+    slug: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    timeToRead: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+
+}
 export default PostItem;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,64 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Home</h1>
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-  </Layout >
-)
+
+
+
+
+const IndexPage = () => {
+
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+  query PostList {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            background
+            category
+            date(formatString: "DD [de] MMM [de] YYYY", locale: "pt-br")
+            description
+            title
+          }
+          timeToRead
+          wordCount {
+            words
+          }
+        }
+      }
+    }
+  }
+  `);
+
+  const postList = allMarkdownRemark.edges;
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Home</h1>
+      {postList.map(({ 
+        node: { 
+          frontmatter: { background, category, date, title, description },
+          timeToRead,
+        }, 
+      }) => (
+        <PostItem
+          slug="/about/"
+          background={background}
+          category={category}
+          date={date}
+          timeToRead={timeToRead}
+          title={title}
+          description={description}
+        />
+      ))}
+
+    </Layout >
+  )
+
+}
 
 export default IndexPage
